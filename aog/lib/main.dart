846c606aabe0813b0fab755ff791e929664ff0c0
MarcLins <marcelo.digital.arts@gmail.com>
2020-02-18 11:17:48 -0300

    First Commit

---
+++ aog/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:aog/widgets/input.widget.dart';
+import 'package:aog/widgets/loading-button.widget.dart';
+import 'package:aog/widgets/logo.widget.dart';
+import 'package:aog/widgets/success.widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Alcool ou Gasolina',
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  var _gasCtrl = new MoneyMaskedTextController();
+  var _alcCtrl = new MoneyMaskedTextController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: ListView(
+        children: <Widget>[
+          Logo(),
+          Success(
+            reset: () {},
+            result: "Compensa Utilizar X",
+          ),
+          Input(label: "Gasolina", ctrl: _gasCtrl),
+          Input(label: "Álcool", ctrl: _alcCtrl),
+          LoadingButton(
+            busy: false,
+            invert: false,
+            func: () {},
+            text: "CALCULAR",
+          )
+        ],
+      ),
+    );
+  }
+}
